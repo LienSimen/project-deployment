aa233d050a1a3515eb97d877b0bede36322f4c07
Dragelol <simenli123@hotmail.com>
2023-11-07 14:21:36 +0100

    Implemented pictureName

--- routes/pictures.js
+++ routes/pictures.js
@@ -16,4 +16,21 @@ router.post("/", function (req, res, next) {
   res.end();
 });
 
+// New GET route for displaying a specific picture by name
+router.get("/:pictureName", function (req, res, next) {
+  var pictureName = req.params.pictureName;
+  var picturePath = path.join(__dirname, "../pictures", pictureName);
+
+  // Check if the file exists in the directory
+  fs.access(picturePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // Handle file not existing
+      console.error(err);
+      return res.status(404).send("Picture not found");
+    }
+    // If the file exists, send it to the client
+    res.sendFile(picturePath);
+  });
+});
+
 module.exports = router;
